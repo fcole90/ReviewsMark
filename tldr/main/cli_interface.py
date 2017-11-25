d8445eb7172c634524c7a6c49ccd5669b4b0f937
Fabio Colella <fcole90@gmail.com>
2017-11-25 21:02:02 +0200

    Added cli support and full pages downloading

--- tldr/main/cli_interface.py
+++ tldr/main/cli_interface.py
@@ -10,5 +10,10 @@ def cli_interface():
         \___)   
  ~~~~~~~~~~~~~~~~~~-->""")
     url = input("Please, paste the URL to the reviews.\n>>> ")
-    soup = amazon.get_soup(url)
+    url = url.strip()
+    for review in amazon.get_all_reviews_in_all_pages(url):
+        print("{}:\t {}".format(review[0], review[1]))
+    print(amazon.get_all_reviews_in_all_pages(url, limit=5))
 
+if __name__ == "__main__":
+    cli_interface()
--- tldr/scraper/amazon_review_retrieval.py
+++ tldr/scraper/amazon_review_retrieval.py
@@ -13,13 +13,41 @@ def get_soup(url):
 def get_reviews_html(soup_page):
     return soup_page.find_all('div', class_="a-section review")
 
-def get_reviews_ratings(soup_reviews_list):
-    return [el.find_all('span', class_="a-icon-alt") for el in soup_reviews_list]
+def get_reviews_ratings(soup_page):
+    return soup_page.find_all('span', class_="a-icon-alt")
 
-def get_reviews_text(soup_reviews_list):
-    return [el.find_all('span', class_="a-size-base review-text") for el in soup_reviews_list]
+def get_reviews_text(soup_page):
+    return soup_page.find_all('span', class_="a-size-base review-text")
 
 def get_last_page_number(soup_page):
     ellippsis_button = soup_page.find_all('li', class_="a-disabled page-ellipsis")[0]
     last_page_button = ellippsis_button.find_next_sibling()
     return last_page_button.get_text()
+
+def get_all_reviews_in_all_pages(url, limit=20):
+
+    # Lambda function to flatten a list (list of list -> list)
+    flatten = lambda l: [item for sublist in l for item in sublist]
+
+    first_page = get_soup(url)
+
+    # Get the last page as an integer
+    last_page = int(get_last_page_number(first_page))
+
+    # List all the pages from 1 to last page
+    pages = list()
+    for i in range(1, min(limit, last_page) + 1):
+        pages.append(get_soup(url+"&pageNumber={}".format(i)))
+
+    # Download the review of all the pages
+    reviews = list()
+    for page in pages:
+        reviews_texts = [r.get_text("|", strip=True) for r in get_reviews_text(page)]
+        reviews_ratings = [r.get_text("|", strip=True) for r in get_reviews_ratings(page)]
+
+        page_reviews = zip(reviews_ratings, reviews_texts)
+        reviews.extend(page_reviews)
+
+    return reviews
+
+
